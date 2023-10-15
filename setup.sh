51f97b5bb0b4db6f7d18e2759272a8a4564e82f8
Toyin Akinmusuru <mrosprey@gmail.com>
2023-10-15 07:38:16 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
+brew install --cask blisk
+brew install --cask vincelwt-chatgpt
+brew install --cask google-chrome
+brew install --cask adobe-creative-cloud
+brew install --cask cyberduck
+brew install --cask dbeaver-community
+brew install --cask diffmerge
+brew install --cask firefox
+brew install --cask gitkraken
+brew install --cask gitkraken-cli
+brew install --cask gyazo
+brew install --cask imageoptim
+brew install --cask insomnia
+brew install --cask keepingyouawake
+brew install lazydocker
+brew install lazygit
+brew install --cask libreoffice
+brew install --cask loom
+brew install --cask menubar-countdown
+brew install --cask notion
+brew install --cask orbstack
+brew install --cask postman
+brew install --cask rectangle
+brew install --cask sequel-pro
+brew install --cask skitch
+brew install --cask skype
+brew install --cask slack
+brew install --cask sublime-text
+brew install --cask tablecruncher
+brew install --cask tableplus
+brew install --cask textmate
+brew install --cask tomighty
+brew install --cask vlc
+brew install --cask visual-studio-code
+brew install wardenenv/warden/warden
+brew install --cask whatsapp
+
+
+
+# brew install --cask phpstorm
