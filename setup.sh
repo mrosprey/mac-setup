821130fc1aa83664178d70d2a56a496a3cda58d4
Toyin Akinmusuru <mrosprey@gmail.com>
2026-01-10 12:43:17 -0600

    Update setup.sh - add cursor, affinity, snagit - in progress

--- setup.sh
+++ setup.sh
@@ -1,34 +1,44 @@
 #!/bin/sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
-brew install --cask blisk
-brew install --cask vincelwt-chatgpt
-brew install --cask google-chrome
-brew install --cask cyberduck
-brew install --cask dbeaver-community
-brew install --cask diffmerge
-brew install --cask firefox
-brew install --cask gitkraken
-brew install --cask gyazo
-brew install --cask imageoptim
-brew install --cask insomnia
-brew install --cask keepingyouawake
-brew install lazydocker
-brew install lazygit
-brew install --cask libreoffice
-brew install --cask loom
-brew install --cask macwhisper
-brew install --cask menubar-countdown
-brew install --cask notion
+
+#Affinity
+brew install --cask affinity             #Affinity
+brew install --cask blisk                #Blisk
+brew install --cask chatgpt              #ChatGPT #license
+brew install --cask google-chrome        #Chrome
+brew install --cask cursor               #Cursor #license
+brew install --cask cyberduck            #Cyberduck
+brew install --cask dbeaver-community    #DBeaver
+brew install --cask diffmerge            #DiffMerge
+brew install --cask firefox              #Firefox
+brew install --cask github
+brew install --cask gitkraken            #
+brew install --cask gyazo                #
+brew install --cask herd                 #
+brew install --cask imageoptim           #
+brew install --cask insomnia             #
+brew install --cask iterm2
+brew install --cask keepingyouawake      #
+brew install lazydocker                  #
+brew install lazygit                     #
+brew install --cask libreoffice          #
+brew install --cask loom                 #
+brew install --cask macwhisper           #
+brew install --cask menubar-countdown    #
+brew install --cask notion               #
+brew install --cask onlyoffice           #
 brew install --cask orbstack
 brew install --cask postman
 brew install --cask rectangle
 brew install --cask rustdesk
 brew install --cask sequel-ace
+brew install --cask selfcontrol
 brew install siege
 brew install --cask skitch
 brew install --cask skype
 brew install --cask slack
+brew install --cask snagit
 brew install --cask sublime-text
 brew install --cask obsidian
 brew install --cask tablecruncher
