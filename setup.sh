0a2ad1e6765a3c8fe76b81534de2ee5c7bd96bd3
Toyin Akinmusuru <mrosprey@gmail.com>
2026-05-22 02:03:07 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,58 +1,59 @@
 #!/bin/sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install git
 
-#Affinity
-brew install --cask affinity             #Affinity
-brew install --cask blisk                #Blisk
-brew install --cask chatgpt              #ChatGPT #license
-brew install --cask google-chrome        #Chrome
-brew install --cask cursor               #Cursor #license
-brew install --cask cyberduck            #Cyberduck
-brew install --cask dbeaver-community    #DBeaver
-brew install --cask diffmerge            #DiffMerge
-brew install --cask firefox              #Firefox
+brew install git
+brew install siege
+brew install wget
+
+brew install --cask affinity
+brew install --cask antigravity
+brew install --cask battle-net
+brew install --cask beekeeper-studio
+brew install --cask blisk
+brew install --cask cavalry
+brew install --cask chatgpt
+brew install --cask claude
+brew install --cask claude-code
+brew install --cask google-chrome
+brew install --cask cursor
+brew install --cask cyberduck
+brew install --cask dbeaver-community
+brew install --cask diffmerge
+brew install --cask docker-desktop
+brew install --cask downie
+brew install --cask firefox
 brew install --cask github
-brew install --cask gitkraken            #
-brew install --cask gyazo                #
-brew install --cask herd                 #
-brew install --cask imageoptim           #
-brew install --cask insomnia             #
+brew install --cask gitkraken
+brew install --cask gyazo
+brew install --cask herd
+brew install --cask insomnia
 brew install --cask iterm2
-brew install --cask keepingyouawake      #
-brew install lazydocker                  #
-brew install lazygit                     #
-brew install --cask libreoffice          #
-brew install --cask loom                 #
-brew install --cask macwhisper           #
-brew install --cask menubar-countdown    #
-brew install --cask notion               #
-brew install --cask onlyoffice           #
+brew install --cask keepingyouawake
+brew install --cask lm-studio
+brew install --cask loom
+brew install --cask notion
+brew install --cask obsidian
+brew install --cask onlyoffice
 brew install --cask orbstack
 brew install --cask postman
 brew install --cask rectangle
 brew install --cask rustdesk
 brew install --cask sequel-ace
 brew install --cask selfcontrol
-brew install siege
-brew install --cask skitch
-brew install --cask skype
 brew install --cask slack
 brew install --cask snagit
 brew install --cask sublime-text
 brew install --cask obsidian
 brew install --cask tablecruncher
 brew install --cask tableplus
-brew install --cask termius
 brew install --cask textmate
-brew install --cask tomighty
-brew install --cask vlc
 brew install --cask visual-studio-code
-brew install wardenenv/warden/warden
-brew install wget
+brew install --cask visualdiffer
+brew install --cask vlc
+brew install --cask voiceink
 brew install --cask whatsapp
+brew install --cask zoom
 
 #install grayscale menubar tool -- https://github.com/brettferdosi/grayscale
 brew install --cask --no-quarantine brettferdosi/tap/grayscale
 
-# brew install --cask phpstorm
